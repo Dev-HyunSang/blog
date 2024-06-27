40f6e2b5a2936949e0a473e01f62735b508ea991
HyunSang Park, To!$F <me@hyunsang.dev>
2024-06-27 07:37:37 +0000

    20240627:1년 동안 GDG 커뮤니티 활동을 하면서

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from 'next/navigation'
 import { CustomMDX } from 'app/components/mdx'
 import { formatDate, getBlogPosts } from 'app/blog/utils'
 import { baseUrl } from 'app/sitemap'
+import { TableOfContents } from 'app/components/toc'
 
 export async function generateStaticParams() {
   let posts = getBlogPosts()
