27e4460217e4e73ec6a6036a329ffbde6739de6e
HyunSang Park <me@hyunsang.dev>
2024-10-05 07:20:24 +0000

    feat: add TIL Link

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -52,6 +52,17 @@ export default function Footer() {
             <p className="ml-2 h-7">Linkedin</p>
           </a>
         </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://til.hyunsang.dev"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">Today HyunSang Learned</p>
+          </a>
+        </li>
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
         © {new Date().getFullYear()} MIT Licensed
