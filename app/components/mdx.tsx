ca495a4023af2972f57b8db32570c1eed4e75241
HyunSang Park <me@hyunsang.dev>
2024-11-23 05:38:25 +0000

    파일명을 한글로도 사용할 수 있도록 slug 코드 개선함.

--- app/components/mdx.tsx
+++ app/components/mdx.tsx
@@ -60,7 +60,7 @@ function slugify(str) {
     .trim() // Remove whitespace from both ends of a string
     .replace(/\s+/g, '-') // Replace spaces with -
     .replace(/&/g, '-and-') // Replace & with 'and'
-    .replace(/[^\w\-]+/g, '') // Remove all non-word characters except for -
+    .replace(/[^\w\-\uac00-\ud7af]+/g, '')
     .replace(/\-\-+/g, '-') // Replace multiple - with single -
 }
 
